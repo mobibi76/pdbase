764278de8406fd95cd825221f6a23e44f79f12d4
mobibi76 <snowfall.hamn@gmail.com>
2024-09-04 02:08:25 +0900

    Delete source/containerStyle.js

--- source/containerStyle.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function adjustContainerHeight() {
-        var headerHeight = document.querySelector('h1').offsetHeight + document.querySelector('h2').offsetHeight;
-        document.querySelector('#container').style.height = `calc(100vh - ${headerHeight}px)`;
-    }
-
-    adjustContainerHeight();
-    window.addEventListener('resize', adjustContainerHeight);
-});
